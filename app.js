b7e8227bc45f962dd8db340ab2239c466079d109
Ultral1sk <jderebanoski@gmail.com>
2020-02-12 13:46:51 +0100

    BACKEND : api/dashboard/ and api/auth routes added

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const connectDB   = require('./config/db');
 
 require('dotenv').config();
 
-
+//DB connection
 connectDB();
 
-app.get('/', ( req, res ) =>  res.send('Hi from HOME PAGE'))
+// Body parser outter middleware of Express
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
 
+app.use('/api/dashboard', require('./router/home'));
+app.use('/api/auth', require('./router/auth'));
+
+//server port loader
 const PORT    = process.env.PORT || 5001
 app.listen(PORT, () => console.log(`CRM APP server is listening the ${PORT} port`));
 
