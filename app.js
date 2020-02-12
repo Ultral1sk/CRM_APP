091e2d70b509299af3acee5a0fb950cdde22126e
Ultral1sk <jderebanoski@gmail.com>
2020-02-12 12:34:17 +0100

    BACKEND : MVC - Model View Controller folder structure added

--- app.js
+++ app.js
@@ -2,5 +2,8 @@ const express = require('express');
 const app     = express();
 const PORT    = process.env.PORT || 5001
 
+
+app.get('/', ( req, res ) =>  res.send('Hi from HOME PAGE'))
+
 app.listen(PORT, () => console.log(`CRM APP server is listening the ${PORT} port`));
 
